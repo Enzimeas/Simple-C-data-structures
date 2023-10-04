716016b7e32373a1c8dc4ffb9bd24aee4bae53a5
Enzo Traldi dos Anjos <86757859+Enzimeas@users.noreply.github.com>
2023-10-03 21:47:37 -0300

    Update linked_list.hpp

--- LinkedLists/linked_list.hpp
+++ LinkedLists/linked_list.hpp
@@ -1,8 +1,6 @@
 #pragma once
 #include <iostream>
 
-
-
 /*
  * @brief Class representing a generic linked list
  * @tparam T type of elements in the list
@@ -33,126 +31,35 @@ public:
    * @brief Default List constructor
    * Starts list as empty
    */
-  List() : head(nullptr){};
+  List();
 
   /*
    * @brief Function that checks if the list is empty
    * @return boolean
    */
-  bool isEmpty()
-  {
-    if ((this->head == nullptr))
-    {
-      return true;
-    }
-    else 
-    {
-      return false;
-    }
-  }
+  bool isEmpty();
 
   /*
    * @brief Function that inserts a element at the end of list
    * @return void
    */
-  void insert(T value)
-  {
-    if (isEmpty())
-    {
-      this->head = new Node(value);
-    }
-    else
-    {
-      Node *current = this->head;
-      while (current->next != nullptr)
-      {
-        current = current->next;
-      }
-      current->next = new Node(value);
-    }
-  }
+  void insert(T value);
 
   /*
    * @brief Function that remove one element of a certain value
    * @return void
    */
-  void remove(T value)
-  {
-    if (isEmpty())
-    {
-      std::cout << "The list is empty\n";
-    }
-    else
-    {
-      Node *current = this->head;
-      Node *previous = nullptr;
-
-      while (current != nullptr)
-      {
-        if (current->value == value)
-        {
-          if (previous == nullptr)
-          {
-            this->head = current->next;
-            delete (current);
-            break;
-          }
-          else
-          {
-            previous->next = current->next;
-            delete (current);
-            break;
-          }
-        }
-        previous = current;
-        current = current->next;
-      }
-    }
-  }
+  void remove(T value);
 
   /*
    * @brief Functions that searches for a element on the list.
    * @return boolean
    */
-  bool search(T value)
-  {
-    if (isEmpty())
-    {
-      return false;
-    }
-    else
-    {
-      Node *current = this->head;
-      while (current != nullptr)
-      {
-        if (current->value == value)
-        {
-          return true;
-        }
-        current = current->next;
-      }
-      return false;
-    }
-  }
+  bool search(T value);
 
   /*
   * @brief Function that prints the list
   * @return void
   */
-  void print()
-  {
-    if (isEmpty())
-    {
-      std::cout << "The list is empty.\n";
-    }
-    else
-    {
-      Node *current = this->head;
-      while (current != nullptr)
-      {
-        std::cout << current->value << std::endl;
-        current = current->next;
-      }
-    }
-  }
+  void print();
 };
